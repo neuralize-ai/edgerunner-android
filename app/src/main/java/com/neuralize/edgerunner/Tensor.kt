0a7dde23af6dce008dfe7e43a76a4a7418281c13
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-06-29 19:08:36 -0700

    Add getBuffer method to Tensor

--- app/src/main/java/com/neuralize/edgerunner/Tensor.kt
+++ app/src/main/java/com/neuralize/edgerunner/Tensor.kt
@@ -1,27 +1,46 @@
 package com.neuralize.edgerunner
 
+import java.nio.ByteBuffer
+
 class Tensor(private val nativeHandle: Long) {
     init {
         System.loadLibrary("tensor_jni")
     }
+
     fun getName(): String {
         return nativeGetName(nativeHandle)
     }
+
     fun getType(): Int {
         return nativeGetType(nativeHandle)
     }
+
     fun getDimensions(): LongArray {
         return nativeGetDimensions(nativeHandle)
     }
+
     fun getSize(): Long {
         return nativeGetSize(nativeHandle)
     }
+
+    fun getBuffer(): ByteBuffer {
+        return nativeGetBuffer(nativeHandle)
+    }
+
     protected fun finalize() {
         nativeDestroy(nativeHandle)
     }
+
     private external fun nativeGetName(nativeHandle: Long): String
+
     private external fun nativeGetType(nativeHandle: Long): Int
+
     private external fun nativeGetDimensions(nativeHandle: Long): LongArray
+
     private external fun nativeGetSize(nativeHandle: Long): Long
+
+    private external fun nativeGetBuffer(nativeHandle: Long): ByteBuffer
+
     private external fun nativeDestroy(nativeHandle: Long)
 }
+
