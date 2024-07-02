770eb544a57d6d246418b3bc5dc4b4753a67f8d8
Ciaran O' Rourke <ciaranborourke-dev@proton.me>
2024-07-01 19:22:33 -0700

    Initialise OpenCV, create model from buffer and load labels in ImageClassifier constructor

--- imageclassifier/src/main/java/com/neuralize/imageclassifier/ImageClassifier.kt
+++ imageclassifier/src/main/java/com/neuralize/imageclassifier/ImageClassifier.kt
@@ -1,13 +1,46 @@
 package com.neuralize.imageclassifier
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 import com.neuralize.edgerunner.Model
+import com.neuralize.edgerunner.Status
+import org.opencv.android.Utils
+import org.opencv.core.Core
+import org.opencv.core.CvType
+import org.opencv.core.Mat
+import org.opencv.core.Rect
+import org.opencv.core.Scalar
+import org.opencv.core.Size
+import org.opencv.imgproc.Imgproc
+import org.opencv.android.OpenCVLoader
+import java.io.IOException
+import java.nio.ByteBuffer
+import kotlin.math.exp
+import kotlin.math.floor
+import kotlin.time.Duration
+import kotlin.time.measureTimedValue
 
-class ImageClassifier(private val modelPath: String) {
-    private var model = Model(modelPath)
+class ImageClassifier(private val context: Context, modelBuffer: ByteBuffer) {
+    private var model: Model
+    private val labelList: List<String>
 
-    fun classify(): String {
-        var result = "no result"
+    init {
+        OpenCVLoader.initLocal()
 
+        model = Model(modelBuffer.asReadOnlyBuffer())
+
+        labelList = loadLabelList("imagenet_labels.txt")
+    }
+
+    fun classify(imageFilename: String): String {
         return result
+    }
+    private fun loadLabelList(labelsFileName: String): List<String> {
+        val assetManager = context.assets
+        val inputStream = assetManager.open(labelsFileName)
+        val labels = inputStream.bufferedReader().use { it.readLines() }
+        return labels
     }
 }
